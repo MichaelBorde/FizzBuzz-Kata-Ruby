de2c2ea43a2f1b3f6dd244f8b0c84095a625c83b
Michael Borde <michael.borde@gmail.com>
2012-03-26 14:52:23 +0200

    first commit

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+﻿class FizzBuzz
+  def enumerate()
+    raw_numbers = (1..100).to_a
+    return raw_numbers.collect {|element| to_fizzbuzz(element)}
+  end
+  
+  private
+  def to_fizzbuzz(number)
+    numberIsMultipleOf = MULTIPLE.curry[number]
+    return "fizzbuzz" if numberIsMultipleOf.(15)
+    return "fizz" if numberIsMultipleOf.(3)
+    return "buzz" if numberIsMultipleOf.(5)
+    return number.to_s
+  end
+   
+  MULTIPLE = lambda {|multiple, number| multiple % number == 0}
+end
---
+++ tc_fizzbuzz.rb
@@ -0,0 +1,29 @@
+﻿require "test/unit"
+require_relative "fizzbuzz"
+
+class FizzBuzzTest < Test::Unit::TestCase
+  def setup
+    @buzzer = FizzBuzz.new
+  end
+  
+  def test_can_enumerate  
+    result =  @buzzer.enumerate
+    
+    assert_equal(100, result.length)
+  end
+  
+  def test_can_say_basic_numbers
+    result = @buzzer.enumerate
+    
+    assert_equal("1", result[0])
+    assert_equal("2", result[1])
+  end
+    
+  def test_can_say_fizzbuzz
+    result = @buzzer.enumerate
+    
+    assert_equal("fizz", result[2])
+    assert_equal("buzz", result[4])
+    assert_equal("fizzbuzz", result[14])
+  end
+end
